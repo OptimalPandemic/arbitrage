9377e8e1edc62b1ef65bce777ba8b92cc530d060
Austin Jones <austin.jones231@gmail.com>
2017-07-07 14:49:28 -0400

    m

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ catch(PDOException $e)
         <center>
         <hr />
         <div class="row thumbnail" id="image-frame">
-            <img src="img/<?= $data['img_url'] ?>"/>
+            <img src="img/<?= $data['img_url'] ?>" style="min-width: 90%"/>
         </div>
         <hr />
         </center>
